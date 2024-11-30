1769c3cfc0035a5bfc32a3d7a53973a7b0afbbc0
michael <michaeleze3@gmail.com>
2024-11-30 11:21:12 +0100

    create theme, types, configs

---
+++ src/constants/constants.ts
@@ -0,0 +1,13 @@
+export const PAGE = {
+  title: ' Star Wars Explorer',
+  subTitle: 'Explore the vast universe of Star Wars characters, planets, and more',
+  charactersSearchBarLabel: 'Search Characters',
+  planetSearchBarLabel: 'Search Planets',
+  backButtonLabel: 'Go Back',
+  homeImageUrl: 'https://starwars-visualguide.com/assets/img/characters/11.jpg',
+  navigation: [
+    { name: 'Home', href: '/' },
+    { name: 'Characters', href: '/characters' },
+    { name: 'Planets', href: '/planets' },
+  ]
+}
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,36 @@
-import { createTheme } from '@mui/material/styles';
+'use client'
 
-const theme = createTheme({
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
-      main: '#1976d2',
+      main: '#FFE81F',
     },
-    secondary: {
-      main: '#dc004e',
+    background: {
+      default: '#191818de',
+      paper: '#1E1E1E',
     },
   },
-});
-
-export default theme;
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    h1: {
+      fontSize: '2.5rem',
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: '2rem',
+      fontWeight: 600,
+    },
+  },
+  components: {
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#1E1E1E',
+        },
+      },
+    },
+  },
+})
